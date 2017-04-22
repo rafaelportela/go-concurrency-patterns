0f3e37538ebf08fd7ab570253de91be3558b70c3
Rafael Portela <rafaelportela90@gmail.com>
2017-04-22 14:34:53 +0200

    New code chapter

---
+++ improvedsub/reader.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Item struct {
+	Title, Channel, GUID string // subset of RSS fields
+}
+
+type Fetcher interface {
+	// Fetches items for a given uri and returns the time when the next
+	// fetch should be attempted.
+	Fetch() (items []Item, next time.Time, err error)
+}
+
+// Subscription delivers Items over a channel.
+// Close cancels the subscription, closes the Updates channel and
+// returns the last fetch error, if any.
+type Subscription interface {
+	Updates() <-chan Item // stream of Items
+	Close() error         // close the stream
+}
+
+func main() {
+
+	// Subscribe to some feeds and create a merged update stream
+	merged := Merge(
+		Subscribe(Fetch("blog.goland.org")),
+		Subscribe(Fetch("googleblog.blogspot.com")),
+		Subscribe(Fetch("googledevelopers.blogspot.com")))
+
+	// Close the subscription after some time
+	time.AfterFunc(3*time.Second, func() {
+		fmt.Println("Merged subsription closed. Errors: ", merged.Close())
+	})
+
+	// Print the stream.
+	// When updates channel is closed, range understands that and for loop ends.
+	for it := range merged.Updates() {
+		fmt.Println(it.Channel, it.Title)
+	}
+
+	time.Sleep(1 * time.Second)
+	fmt.Println("End of main")
+
+	panic("Show me the stacks")
+}
